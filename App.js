29a75b3f11f9419e8113ecc6320cde606ec08628
Sue Ann Ioanis <sueann.ioanis@gmail.com>
2017-05-06 21:11:31 -0400

    Add initial react native build stuff.

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Tumfoodery</Text>
+        <Text>The "Dating App" for Food</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
